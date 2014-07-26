25f2288e1a53252f31e394c9e3c9ee9ca200bcc0
tomblack1 <tomblack_1@yahoo.com>
2014-07-26 15:01:45 -0500

    Assignment 2 - Matrix Caching
    Functions to do inverse of matrix and pull from cache if already set.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+##
+## Overall:  Functions load matrix and solve for inverse if not 
+## already set in session cache, otherwise it solves for inverse
+## and sets in cache.
 
 ## Write a short comment describing this function
-
+##
+## Function takes a matrix and essentially builds a class
+## with get, set, getinverse and setinverse functions to 
+## reset and read from a cached copy of already solved 
+## matrix inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  mx <- NULL
+  set <- function(y){
+      x <<-y
+      mx <<-NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) mx <<- solve
+  getinverse <- function() mx
+  
+  list(set = set, get = get, 
+       setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+##
+## Function checks the cache for the inverse already being set
+## If inverse not already set, then solves for inverse and sets 
+## it into the cache for possible use later.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    mx <- x$getinverse()
+    if(!is.null(mx)){
+      message("getting cached data")
+      return(mx)
+    }
+    data <- x$get()
+    mx <- solve(data)
+    x$setinverse(mx)
+    
+    mx
 }
